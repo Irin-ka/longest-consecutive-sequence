2e79806695e950a8a4ff586a166c6cbe2ccd8cd6
ira <irin-ka@inbox.ru>
2018-10-07 22:00:56 +0300

    massive

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+    if(array.length==0) return 0;
+    //array = array.sort(function(a,b){return b-a});
+    var arr = [[array[0], array[0]]]; 
+    for (var i=1;i<array.length;i++){ 
+      var v=array[i];
+      var found =0; 
+      for (var j=0;j<arr.length;j++){ 
+          if((arr[j][0]-1) == v){ 
+            arr[j][0]= v; 
+            found=1; 
+          }
+          else if((arr[j][1]+1)==v){
+            arr[j][1]= v; 
+            found=1; 
+          } 
+      } 
+      if(!found){ 
+            arr.push([v, v]); 
+      } 
+    } 
+    var min =arr[0][0], max = arr[0][1]; 
+    for (var j=1;j<arr.length;j++){
+        if(arr[j][0]<arr[j][1]){
+              if(max == arr[j][0]){ 
+                max = arr[j][1]; 
+              }
+              else if(min == arr[j][1]){ 
+                min = arr[j][0]; 
+              } 
+              else if((max- min) < (arr[j][1] - arr[j][0])){ 
+                max = arr[j][1]; 
+                min = arr[j][0]; 
+              } 
+        } 
+    } 
+          //console.log(arr); 
+          console.log(min,max,max-min+1);
+          
+          return max - min+1;
 }
